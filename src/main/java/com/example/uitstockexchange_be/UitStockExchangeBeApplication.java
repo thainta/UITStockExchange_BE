d2e2910284f1e89201aed9a0924b6d31a3cfd521
thainta <thai16122001@gmail.com>
2022-03-23 10:54:24 +0700

    Init Project

---
+++ src/main/java/com/example/uitstockexchange_be/UitStockExchangeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.uitstockexchange_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitStockExchangeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UitStockExchangeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uitstockexchange_be/UitStockExchangeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uitstockexchange_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UitStockExchangeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
